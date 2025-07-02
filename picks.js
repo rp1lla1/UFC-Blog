07a7b2aef75eae7c0cd3f136f52d8f03c46bd834
rp1lla1 <roshan2pillai@gmail.com>
2025-07-02 16:13:45 -0400

    picks

--- picks.js
+++ picks.js
@@ -1,11 +1,11 @@
 const picksData = {
-  total: 2,
-  correct: 0,
+  total: 4,
+  correct: 1,
   history: [
     { event: "UFC 316", matchup: "Sean O'Malley vs Merab Dvalishvili", pick: "O'Malley", result: false },
     { event: "UFC Fight Night", matchup: "Kamaru Usman vs Joaquin Buckley", pick: "Buckley", result: false },
     { event: "UFC Fight Night", matchup: "Khalil Rountree vs Jamahal Hill", pick: "Rountree", result: true },
-    { event: "UFC 317", matchup: "Charles Oliveira vs Ilia Topuria", pick: "Oliveira", result: true }
+    { event: "UFC 317", matchup: "Charles Oliveira vs Ilia Topuria", pick: "Oliveira", result: false }
     
   ]
 };
